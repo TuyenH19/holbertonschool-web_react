1d5a7159fbce96c62feac915adcbacde6c2e9b47
TuyenH <thtuyen199@gmail.com>
2025-10-31 15:02:03 +0000

    Task2 - Embedding expressions, functions

---
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+import holbertonLogo from './assets/holberton-logo.jpg';
+import { getCurrentYear, getFooterCopy } from './utils.js';
+import Notifications from './Notifications.jsx';
+
+function App() {
+  return (
+    <>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={holbertonLogo} alt="holberton logo" />
+          <h1>School dashboard</h1>
+        </header>
+        <div className="App-body">
+          <p>Login to access the full dashboard</p>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          <button type="submit">OK</button>
+        </div>
+        <div className="App-footer">
+          <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
