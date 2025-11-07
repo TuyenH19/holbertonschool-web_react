0b85f204f18521523dc1c2683b11e894a07aea6b
TuyenH <thtuyen199@gmail.com>
2025-11-07 09:07:59 +0000

    separate element email and password

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -16,11 +16,15 @@ function App() {
         </header>
         <div className="App-body">
           <p>Login to access the full dashboard</p>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" />
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
-          <button type="submit">OK</button>
+          <div className="input-group email">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="input-group password">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <button type="button">OK</button>
         </div>
         <div className="App-footer">
           <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
