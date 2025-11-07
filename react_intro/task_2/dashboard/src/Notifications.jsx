4a342487e1e69e149880bcb75d334e5141852e00
TuyenH <thtuyen199@gmail.com>
2025-11-07 14:11:26 +0000

    Task5- update the notification

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -1,9 +1,24 @@
+import closeIcon from './assets/close-button.png';
+import { getLatestNotification } from './utils.js';
 import './Notifications.css';
 
 function Notifications() {
   return (
     <div className="notification-items">
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
+
+      <button
+        aria-label="Close"
+        style={{ float: 'right' }}
+        onClick={() => console.log('Close button has been clicked')}
+      >
+        <img onScroll={closeIcon} alt="close" style={{ height: 20, width: 20 }} />
+      </button>
     </div>
   );
 }
