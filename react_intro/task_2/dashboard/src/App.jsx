c992a032ae6c839af3a03763ca09bae966fa64ba
TuyenH <thtuyen199@gmail.com>
2025-11-13 23:42:57 +0000

    add form into function

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         </header>
         <div className="App-body">
           <p>Login to access the full dashboard</p>
+          <form>
             <div className="input-group">
               <label htmlFor="email">Email:</label>
               <input type="email" id="email" name="email" />
@@ -24,7 +25,8 @@ function App() {
               <label htmlFor="password">Password:</label>
               <input type="password" id="password" name="password" />
             </div>
-          <button type="button">OK</button>
+            <button type="button">OK</button>
+            </form>
         </div>
         <div className="App-footer">
           <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
