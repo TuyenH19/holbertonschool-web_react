79097dfab8f56854a334a56319a61768bc76e7bd
TuyenH <thtuyen199@gmail.com>
2025-11-24 10:28:58 +0000

    Task0- Switch to class components

---
+++ react_component/task_0/dashboard/src/CourseList/CourseList.jsx
@@ -0,0 +1,34 @@
+import CourseListRow from './CourseListRow';
+
+function CourseList({ courses = [] }) {
+  if (courses.length === 0) {
+    return (
+      <table id="CourseList">
+        <tbody>
+          <CourseListRow isHeader={false} textFirstCell="No course available yet" />
+        </tbody>
+      </table>
+    );
+  }
+
+  return (
+    <table id="CourseList">
+      <thead>
+        <CourseListRow isHeader={true} textFirstCell="Available courses" />
+        <CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
+      </thead>
+      <tbody>
+        {courses.map((course) => (
+          <CourseListRow 
+            key={course.id}
+            isHeader={false}
+            textFirstCell={course.name}
+            textSecondCell={course.credit}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default CourseList;
