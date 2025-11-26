2979c4553e79e0726690cff727845c65dcfad810
TuyenH <thtuyen199@gmail.com>
2025-11-26 15:28:49 +0000

    add tailwind plugin to PostCSS

--- react_styling/task_0/dashboard/vite.config.js
+++ react_styling/task_0/dashboard/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   server: {
     host: true, // Expose to all network interfaces
     port: 5173,  // Specify port
