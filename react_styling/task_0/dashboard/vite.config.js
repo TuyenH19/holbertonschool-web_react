3db4823f31fb5e347f6a62760701a93676efa368
TuyenH <thtuyen199@gmail.com>
2025-11-26 15:39:49 +0000

    update dependency version

--- react_styling/task_0/dashboard/vite.config.js
+++ react_styling/task_0/dashboard/vite.config.js
@@ -4,12 +4,7 @@ import tailwindcss from '@tailwindcss/postcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
+  plugins: [react(), tailwindcss()],
   server: {
     host: true, // Expose to all network interfaces
     port: 5173,  // Specify port
