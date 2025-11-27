dd36e096c0648b350a453907aa527328df422d78
TuyenH <thtuyen199@gmail.com>
2025-11-27 12:59:56 +0000

    change position of main file

--- react_styling/task_2/dashboard/src/main.jsx
+++ react_styling/task_2/dashboard/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './main.css'
+import '../main.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
