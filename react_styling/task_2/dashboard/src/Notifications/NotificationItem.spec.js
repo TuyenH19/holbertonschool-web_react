875e8757dccb76ef79183744a93365b58ed57ebe
TuyenH <thtuyen199@gmail.com>
2025-11-27 12:55:11 +0000

    remove styles tests

--- react_styling/task_2/dashboard/src/Notifications/NotificationItem.spec.js
+++ react_styling/task_2/dashboard/src/Notifications/NotificationItem.spec.js
@@ -4,23 +4,23 @@ import '@testing-library/jest-dom';
 import NotificationItem from './NotificationItem';
 
 describe('NotificationItem Component', () => {
-  test('renders li element with blue color and data-notification-type set to default when type prop is "default"', () => {
-    render(<NotificationItem type="default" value="New course available" />);
-    const listItem = screen.getByRole('listitem');
+  // test('renders li element with blue color and data-notification-type set to default when type prop is "default"', () => {
+  //   render(<NotificationItem type="default" value="New course available" />);
+  //   const listItem = screen.getByRole('listitem');
     
-    expect(listItem).toBeInTheDocument();
-    expect(listItem).toHaveStyle({ color: 'rgb(0, 0, 255)' });
-    expect(listItem).toHaveAttribute('data-notification-type', 'default');
-  });
+  //   expect(listItem).toBeInTheDocument();
+  //   expect(listItem).toHaveStyle({ color: 'rgb(0, 0, 255)' });
+  //   expect(listItem).toHaveAttribute('data-notification-type', 'default');
+  // });
 
-  test('renders li element with red color and data-notification-type set to urgent when type prop s "urgent"', () => {
-    render (<NotificationItem type="urgent" value="New course available" />);
-    const listItem = screen.getByRole('listitem');
+  // test('renders li element with red color and data-notification-type set to urgent when type prop s "urgent"', () => {
+  //   render (<NotificationItem type="urgent" value="New course available" />);
+  //   const listItem = screen.getByRole('listitem');
 
-    expect(listItem).toBeInTheDocument();
-    expect(listItem).toHaveStyle({ color: 'rgb(255, 0, 0)' });
-    expect(listItem).toHaveAttribute('data-notification-type', 'urgent');
-  });
+  //   expect(listItem).toBeInTheDocument();
+  //   expect(listItem).toHaveStyle({ color: 'rgb(255, 0, 0)' });
+  //   expect(listItem).toHaveAttribute('data-notification-type', 'urgent');
+  // });
 
   test('calls markAsRead with correct id when notification item is clicked', () => {
     const markAsReadMock = jest.fn();
