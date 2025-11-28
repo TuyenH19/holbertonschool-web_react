7c165701dc57e4bddf675ffd6a699b88a5c7a467
TuyenH <thtuyen199@gmail.com>
2025-11-28 12:45:57 +0000

    remove border surplus

--- react_styling/task_3/dashboard/src/BodySection/BodySection.jsx
+++ react_styling/task_3/dashboard/src/BodySection/BodySection.jsx
@@ -4,7 +4,7 @@ class BodySection extends Component {
   render() {
     const { title, children } = this.props;
     return (
-      <div className="px-5 pb-2.5 mb-5 border-b-[3px] border-[var(--main-color)]">
+      <div className="px-5 pb-2.5 mb-5 border-b-[3px] border-[var(--main-color)] pt-60">
         <h2 className="text-xl font-bold mb-2.5">{title}</h2>
         {children}
       </div>
--- react_styling/task_3/dashboard/src/Footer/Footer.jsx
+++ react_styling/task_3/dashboard/src/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { getCurrentYear, getFooterCopy } from '../utils/utils';
 
 function Footer() {
   return (
-    <footer className="text-center p-4 border-t-[3px] border-[var(--main-color)] italic mt-auto pt-2.5">
+    <footer className="text-center p-4 italic mt-auto pt-2.5">
       <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
     </footer>
   );
