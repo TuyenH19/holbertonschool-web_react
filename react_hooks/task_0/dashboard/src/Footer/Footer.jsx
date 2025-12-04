184d22a1450e8c47ccc39f5e2d9f8c800918330a
TuyenH <thtuyen199@gmail.com>
2025-12-04 13:02:38 +0000

    feat: use useContext hook to enable context consumption properly.

--- react_hooks/task_0/dashboard/src/Footer/Footer.jsx
+++ react_hooks/task_0/dashboard/src/Footer/Footer.jsx
@@ -1,20 +1,19 @@
+import React, { useContext } from 'react';
 import { getCurrentYear, getFooterCopy } from '../utils/utils';
 import AppContext from '../Context/context';
 
 function Footer() {
+  const { user } = useContext(AppContext);
+
   return (
-    <AppContext.Consumer>
-      {({ user }) => (
-        <footer className="text-center p-4 italic mt-auto pt-2.5 max-[912px]:p-3 max-[912px]:text-sm">
-          <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
-          {user.isLoggedIn && (
-            <p>
-              <a href="#" className="text-[var(--main-color)]">Contact us</a>
-            </p>
-          )}
-        </footer>
+    <footer className="text-center p-4 italic mt-auto pt-2.5 max-[912px]:p-3 max-[912px]:text-sm">
+      <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
+      {user.isLoggedIn && (
+        <p>
+          <a href="#" className="text-[var(--main-color)]">Contact us</a>
+        </p>
       )}
-    </AppContext.Consumer>
+    </footer>
   );
 }
 
