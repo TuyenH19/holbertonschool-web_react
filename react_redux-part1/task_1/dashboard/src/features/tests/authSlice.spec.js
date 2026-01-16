553e86bf77e613ceee69ba3e15b05b52440974be
TuyenH <thtuyen199@gmail.com>
2026-01-16 09:24:22 +0000

    fix: change structure of the test file

--- react_redux-part1/task_1/dashboard/src/features/tests/authSlice.spec.js
+++ react_redux-part1/task_1/dashboard/src/features/tests/authSlice.spec.js
@@ -1,52 +1,58 @@
 import authReducer, { login, logout } from '../auth/authSlice';
 
 describe('authSlice', () => {
-  it('should return the correct initial state by default', () => {
-    const initialState = {
-      user: {
-        email: '',
-        password: '',
-      },
-      isLoggedIn: false,
-    };
+  describe('initialState', () => {
+    it('should return the correct initial state by default', () => {
+      const initialState = {
+        user: {
+          email: '',
+          password: '',
+        },
+        isLoggedIn: false,
+      };
 
-    expect(authReducer(undefined, { type: 'unknown' })).toEqual(initialState);
+      expect(authReducer(undefined, { type: 'unknown' })).toEqual(initialState);
+    });
   });
 
-  it('should update the state correctly when the login action is dispatched', () => {
-    const previousState = {
-      user: {
-        email: '',
-        password: '',
-      },
-      isLoggedIn: false,
-    };
+  describe('login action', () => {
+    it('should update the state correctly when the login action is dispatched', () => {
+      const previousState = {
+        user: {
+          email: '',
+          password: '',
+        },
+        isLoggedIn: false,
+      };
 
-    const payload = {
-      email: 'test@example.com',
-      password: 'password123',
-    };
-
-    const newState = authReducer(previousState, login(payload));
-
-    expect(newState.user.email).toBe('test@example.com');
-    expect(newState.user.password).toBe('password123');
-    expect(newState.isLoggedIn).toBe(true);
-  });
-
-  it('should reset the state correctly when the logout action is dispatched', () => {
-    const previousState = {
-      user: {
+      const payload = {
         email: 'test@example.com',
         password: 'password123',
-      },
-      isLoggedIn: true,
-    };
+      };
 
-    const newState = authReducer(previousState, logout());
+      const newState = authReducer(previousState, login(payload));
 
-    expect(newState.user.email).toBe('');
-    expect(newState.user.password).toBe('');
-    expect(newState.isLoggedIn).toBe(false);
+      expect(newState.user.email).toBe('test@example.com');
+      expect(newState.user.password).toBe('password123');
+      expect(newState.isLoggedIn).toBe(true);
+    });
+  });
+
+  describe('logout action', () => {
+    it('should reset the state correctly when the logout action is dispatched', () => {
+      const previousState = {
+        user: {
+          email: 'test@example.com',
+          password: 'password123',
+        },
+        isLoggedIn: true,
+      };
+
+      const newState = authReducer(previousState, logout());
+
+      expect(newState.user.email).toBe('');
+      expect(newState.user.password).toBe('');
+      expect(newState.isLoggedIn).toBe(false);
+    });
   });
 });
